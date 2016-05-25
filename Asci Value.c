6b97e3720e2685f8d1299e234bdd5461ca5c26f1
petabyteboy <asbhasin123@gmail.com>
2016-05-25 22:35:03 +0530

    Find the Asci Value of a character

---
+++ Asci Value.c
@@ -0,0 +1,13 @@
+//Find the asci value of a character
+#include<stdio.h>
+
+int main(){
+
+	char temp;
+	for(int i=1;i>0;i++){
+	scanf("%c",&temp);
+	printf("Character = %c  Asci Value = %d\n",temp,temp);
+	}
+	return 0;
+
+}
